ccfbc77bb1c6a00e264c537fe22a7e27b02be3d8
Roberto <robertoanfuso98@gmail.com>
2021-05-31 16:08:39 +0200

    add esercizio finito

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -8,8 +8,10 @@ listacognomi.push(cognome);
 
 listacognomi.sort();
 
+var pos = listacognomi.indexOf(cognome);
+
 for(var i=0 ; i<listacognomi.length; i++){
     document.getElementById("cognomi").innerHTML+= " " + listacognomi[i];
-    console.log(listacognomi[i]);
 }
 
+document.getElementById("pos").innerHTML= pos+1;
--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 var myemail= prompt('inserisci la tua email');
-console.log(myemail);
+
 
 var whitelist= ["carlo@live.it" , "maurizio@live.it" , "franco@live.it" , "roberto@live.it"] ;
 
 
+var find=false;
 
 
-
-for(var i=0 ; i < whitelist.length; i++) {
+for(var i=0 ; i < whitelist.length && find==false; i++) {
     if(myemail == whitelist[i]){
-        document.getElementById("trovato").innerHTML = "ho trovato la tua email in posizione " + i;
+       find=true;
         
     }
+}
+
+if(find==true){
+    document.getElementById("trovato").innerHTML = "ho trovato la tua email in posizione " + i;
+}
+else{
+    document.getElementById("trovato").innerHTML = "non ho trovato la tua email";
 }
